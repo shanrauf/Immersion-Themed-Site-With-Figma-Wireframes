3fee84535a681396d3fde8556179300489344d8c
Shan Rauf <s.rauf124@gmail.com>
2020-03-13 21:30:08 -0700

    Add color to footer social icons

--- components/BaseFooter.vue
+++ components/BaseFooter.vue
@@ -2,17 +2,37 @@
   <v-footer padless class="base-footer">
     <v-card class="flex" flat tile>
       <v-card-title>
-        <strong class="subheading"
+        <strong class="copyright"
           >Copyright &copy; {{ new Date().getFullYear() }} Mass Immersion
           Approach</strong
         >
 
         <v-spacer />
 
-        <font-awesome-icon :icon="['fab', 'youtube']" />
-        <font-awesome-icon :icon="['fab', 'twitter']" />
-        <font-awesome-icon :icon="['fab', 'patreon']" />
-        <font-awesome-icon :icon="['fas', 'coffee']" />
+        <div class="social-icons">
+          <font-awesome-icon
+            class="social-icon"
+            :icon="['fab', 'youtube']"
+            :style="{ color: '#FF0000' }"
+          />
+          <font-awesome-icon
+            class="social-icon"
+            :icon="['fab', 'twitter']"
+            :style="{ color: '#1DA1F2' }"
+          />
+          <font-awesome-icon
+            class="social-icon"
+            :icon="['fab', 'patreon']"
+            :style="{ color: '#f96854' }"
+          />
+          <font-awesome-icon
+            class="social-icon"
+            :icon="['fas', 'coffee']"
+            :style="{
+              color: '#29ABE0'
+            }"
+          />
+        </div>
       </v-card-title>
     </v-card>
   </v-footer>
@@ -21,5 +41,16 @@
 <style lang="scss" scoped>
 .base-footer {
   border-top: 1px solid gray;
+}
+.copyright {
+  font-size: 15px;
+}
+.social-icons {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.social-icon {
+  margin: 0 10px;
 }
 </style>
